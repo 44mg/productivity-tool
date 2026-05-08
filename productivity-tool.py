66612ade1c1247c9c0ba2cf60cae52b3a5fd750a
44mg <zsolt.csott@icloud.com>
2026-05-08 20:24:43 +0100

    reset button, pause/resume button

--- productivity-tool.py
+++ productivity-tool.py
@@ -2,7 +2,6 @@ import tkinter as tk
 import customtkinter as ctk
 import json
 import os
-import time
 BG = "#0A84FF"
 
 running = False
@@ -23,15 +22,15 @@ listbox.pack(fill = "both", expand = True , padx = 10, pady = 10)
 entry = ctk.CTkEntry(tabview.tab("To Do"), corner_radius = 10)
 entry.pack(fill = "x", padx = 10, pady = 10)
 
-timer_label = ctk.CTkLabel(tabview.tab("Pomodoro Timer"), text = "25:00")
+timer_label = ctk.CTkLabel(tabview.tab("Pomodoro Timer"), text = "25:00", font = ("Arial", 48))
 timer_label.pack(fill = "both", expand = True, padx= 10, pady = 10)
 
-
+# todo list functions
 
 def add_task():
     task = entry.get()
     listbox.insert(tk.END, task)
-    save_tasks(fill = "x", padx = 10, pady = 10)
+    save_tasks()
 
 def rem_task():
     selected = listbox.curselection()
@@ -51,9 +50,17 @@ def load_tasks():
             for task in tasks:
                 listbox.insert(tk.END, task)
 
+# pomodoro functions
+
+current_minutes = 25
+current_seconds = 0
+
 def tick(minutes, seconds):
+    global current_minutes, current_seconds
     if not running:
         return
+    current_minutes = minutes
+    current_seconds = seconds
     timer_label.configure(text=f"{minutes:02d}:{seconds:02d}")
     if minutes == 0 and seconds == 0:
         timer_label.configure(text="Break Time!")
@@ -70,11 +77,23 @@ def start_timer():
     running = True
     tick(25, 0)
 
-def pause_timer():
+def toggletimer():
+    global running
+    if running:
+        running = False
+        pause_btn.configure(text = "Resume")
+    else:
+        running = True
+        tick(current_minutes, current_seconds)
+        pause_btn.configure(text = "Pause")
+
+def reset_timer():
     global running
     running = False
+    pause_btn.configure(text = "Pause")
+    timer_label.configure(text = "25:00")
 
-
+# todo list
 
 addtask = ctk.CTkButton(tabview.tab("To Do"), text = "Add Task", command = add_task, fg_color=BG)
 addtask.pack(fill = "x", padx = 10, pady = 10)
@@ -82,14 +101,19 @@ addtask.pack(fill = "x", padx = 10, pady = 10)
 remtask = ctk.CTkButton(tabview.tab("To Do"), text = "Remove Task", command = rem_task, fg_color=BG)
 remtask.pack(fill = "x", padx = 10, pady = 10)
 
+# pomodoro timer
+
 start_btn = ctk.CTkButton(tabview.tab("Pomodoro Timer"), text = "Start", command = start_timer )
 start_btn.pack(fill = "x", padx = 10, pady = 10)
 
-pause_btn = ctk.CTkButton(tabview.tab("Pomodoro Timer"), text = "Pause", command = pause_timer)
+pause_btn = ctk.CTkButton(tabview.tab("Pomodoro Timer"), text = "Pause", command = toggletimer)
 pause_btn.pack(fill = "x", padx = 10, pady = 10)
 
+reset_btn = ctk.CTkButton(tabview.tab("Pomodoro Timer"),  text = "Reset", command = reset_timer)
+reset_btn.pack(fill = "x", padx = 10, pady = 10)
 
 
-root.configure(bg=BG)
+
+root.configure(fg_color=BG)
 load_tasks()
 root.mainloop()
